aca4f48181fefce6b09d847b1d619aaf1b6f5f3a
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2024-01-06 11:03:24 -0700

    Order controller being updated

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,4 +1,4 @@
-import asyncHandler from "../middleware/asyncHandler";
+import asyncHandler from "../middleware/asyncHandler.js";
 import order from "../models/orderModel.js";
 
 // @desc create new order
@@ -6,7 +6,40 @@ import order from "../models/orderModel.js";
 //@access Private
 
 const addOrderItems = asyncHandler(async (req, res) => {
-  res.send("Add order items");
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+  } else {
+    // this will put it in the variables but will not be saved to the database
+    const order = new order({
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        product: x._id,
+        _id: undefined,
+      })), // we have to put directly orderItems beacuse if we check orderModel.js order items array data is going to come through directly from front end. But the product which is just the id of the item for each item.
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    // we want to await for for the order and let the data comes and then save it to the database
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  }
 });
 
 // @desc Get logged in user orders
@@ -30,7 +63,7 @@ const getOrderById = asyncHandler(async (req, res) => {
 //@access Private
 
 // by default in orderModel.js isPaid is set default to unpaid
-const udpateOrderToPaid = asyncHandler(async (req, res) => {
+const updateOrderToPaid = asyncHandler(async (req, res) => {
   res.send("update order to paid");
 });
 
@@ -40,3 +73,21 @@ const udpateOrderToPaid = asyncHandler(async (req, res) => {
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
   res.send("update order to delivered");
 });
+
+// @desc Get all orders
+// @route GET /api/orders/
+// @access Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+  res.send("get all orders");
+});
+
+export {
+  addOrderItems,
+  getMyOrders,
+  getOrderById,
+  getOrders,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+};
+
+// being imported to orderRoutes
